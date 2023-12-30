e5598e065059cf7c12ffdb2ab5a639a599432fb2
jhkim <mineship0505@naver.com>
2023-12-30 23:07:08 +0900

    first commit

---
+++ sequences.py
@@ -0,0 +1,9 @@
+# list - sequence of mutable values
+# tuple - sequence of immutable values
+# set - collection of lunique values
+# dict - collection of key-value pairs
+
+coordinateX = 10.0
+coordinateY = 20.0
+
+coordinate = (10.0, 20.0)
