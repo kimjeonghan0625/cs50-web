7417235daf3b733adb7220e90fa3c6a9969cc9c5
jh <mineship@pusan.ac.kr>
2024-01-02 12:28:24 +0900

    until django layout

--- lecture3/lecture3/urls.py
+++ lecture3/lecture3/urls.py
@@ -18,6 +18,8 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('hello/', include("hello.urls"))
+    path("admin/", admin.site.urls),
+    path("hello/", include("hello.urls")),
+    path("newyear/", include("newyear.urls")),
+    path("tasks/", include("tasks.urls")),
 ]
